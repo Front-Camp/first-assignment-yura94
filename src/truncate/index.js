9193a6d06e77826f59563a36628c14d74984fa03
Yurii Lenko <yura.lenko94@gmail.com>
2018-09-13 20:28:22 +0300

    hmw

--- src/truncate/index.js
+++ src/truncate/index.js
@@ -8,7 +8,8 @@
  * @return {string} truncated string
  */
 const truncate = (str, length = 0, replacer = '...') => {
-  /* your logic here...*/
+  if (str.length == 0 || str.length < length) return str
+  return str.substring(0, str.length - replacer.length).concat(replacer)
 };
 
 export default truncate;
--- src/twoSums/index.js
+++ src/twoSums/index.js
@@ -9,7 +9,10 @@
  * twoSums([5, 9, 3], 12); // [9, 3]
  */
 const twoSums = (arr, base) => {
-  /* your logic here...*/
+  let a = 0;
+  for (let i = 0; i < arr.length; i++) {
+    a += arr[i];
+  }
 };
 
 export default twoSums;
